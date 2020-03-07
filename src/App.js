dacaa1404a34ea830a0c9aeba24ad202f7ab5474
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2020-03-07 07:01:31 +0000

    Video Page Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
+import { SearchBar, VideoDetail, VideoList } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+	
+	state={
+		videos: [],
+		selectedVideo: null,
+	}
+	
+	componentDidMount(){
+		this.handleSubmit('Youtube')
+	}
+
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video });	
+	}
+	
+	
+	handleSubmit = async (searchTerm) => {
+		const response = await youtube.get('search', { 
+			params: {
+				part: 'snippet',
+				maxResults: 5,
+				key: '[API KEY]',
+				q: searchTerm,
+			}
+		});
+		
+		this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+	}
+	
+	render(){
+		const{ selectedVideo, videos } = this.state;
+		return(
+		<Grid justify="center" container spacing={16}>
+			<Grid item xs={12}>
+				<Grid container spacing={10}>
+					<Grid item xs={12}>
+						<SearchBar onFormSubmit={this.handleSubmit}/>
+					</Grid>
+					<Grid item xs={8}>
+						<VideoDetail videos={ selectedVideo }/>
+					</Grid>
+					<Grid item xs={4}>
+						<VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+					</Grid>
+					
+				</Grid>
+			</Grid>		
+		</Grid>
+		)
+			
+	}
 }
 
-export default App;
+export default App
